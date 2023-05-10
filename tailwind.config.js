16d91a2cfbdd0f8e258a3ee2d0f26c29955305a7
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2023-05-10 09:00:02 +0500

    fixed some card styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        black:"#000000",
-        main_violet:"#4E0F8A",
-        check_gray:"#DCDDDD",
-        brown_light:"#F0CEAF",
-        card_yellow:"#F0CEAF",
-        card_violet:"#DCCFE8"
+      colors: {
+        black: "#000000",
+        main_violet: "#4E0F8A",
+        check_gray: "#DCDDDD",
+        brown_light: "#F0CEAF",
+        card_yellow: "#F0CEAF",
+        card_violet: "#DCCFE8",
       },
     },
     screens: {
@@ -27,7 +27,7 @@ module.exports = {
       md: "768px",
       lg: "1024px",
       xl: "1440px",
-      xxl:"2000px",
+      xxl: "2000px",
     },
     container: {
       center: true,
@@ -48,7 +48,7 @@ module.exports = {
         // xxl:"2000px",
         // xl: "1200px",
       },
-    }
+    },
   },
   plugins: [],
-}
+};
